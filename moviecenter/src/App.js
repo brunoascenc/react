4734693e3b2112294ac3e87262ada3ca3652d0f8
Bruno Ascenca <brunoascenc1@outlook.com>
2019-05-29 12:35:15 -0300

    movie project

---
+++ moviecenter/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Nav from './Nav'
+import SideNav from './SideNav'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ moviecenter/src/SideNav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function SideNav() {
+  return (
+    <div>
+      <header>
+        <h2>Logo</h2>
+        <nav>
+          <ul>
+            <li>Login</li>
+            <li>Cadastrar</li>
+          </ul>
+        </nav>
+        <div>
+          <input type="text" placeholder="Pesquise..." />
+          <button>Pesquisar</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default SideNav;
